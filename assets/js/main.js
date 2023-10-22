3fd36eec791d2f854aef55803d76c4870ba3e1d6
devsingh1995 <148577233+devsingh1995@users.noreply.github.com>
2023-10-22 12:57:55 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -280,7 +280,7 @@
    */
   window.addEventListener('load', () => {
     AOS.init({
-      duration: 1000,
+      duration: 700,
       easing: 'ease-in-out',
       once: true,
       mirror: false
